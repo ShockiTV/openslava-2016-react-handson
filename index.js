89441da00ca25c6fb673138b241a022d46cf8843
Dusan Hlavaty <dusan.hlavaty@piano.io>
2016-10-07 16:46:35 +0200

    Step 5 - selecting square works

--- index.js
+++ index.js
@@ -40,6 +40,10 @@ var Square = React.createClass({
     var fill = this.props.isBlack ? 'black' : 'white';
     var stroke = this.props.isBlack ? 'white' : 'black';
 
+    if (this.props.isSelected) {
+        fill = 'green';
+    }
+
     return (
         <div style={{
             backgroundColor: fill,
@@ -54,18 +58,25 @@ var Square = React.createClass({
 });
 
 var Board = React.createClass({
+  handleSquareClick: function(x, y) {
+      this.props.onClick(x, y);
+  },
+
   renderSquare: function (i) {
       var x = i % 8;
       var y = Math.floor(i / 8);
       var isBlack = (x + y) % 2 === 1;
 
-      var pawnX = this.props.pawnPosition[0];
-      var pawnY = this.props.pawnPosition[1];
-      var piece = (x === pawnX && y === pawnY) ? <Pawn /> : <div>&nbsp;</div>;
+      var isSelected = (i === this.props.selected);
+      var piece = (this.props.boardData[i] === 'w') ? <Pawn /> : <div>&nbsp;</div>;
 
       return (
-        <div key={i} style={{ width: '12.5%', height: '12.5%' }}>
-            <Square isBlack={isBlack}>
+        <div
+            key={i}
+            style={{ width: '12.5%', height: '12.5%' }}
+            onClick={this.handleSquareClick.bind(this, x, y)}
+        >
+            <Square isBlack={isBlack} isSelected={isSelected}>
               {piece}
             </Square>
         </div>
@@ -91,23 +102,38 @@ var Board = React.createClass({
   }
 });
 
-// ReactDOM.render(<Pawn />, document.getElementById('app'));
-// ReactDOM.render(
-//     <Board pawnPosition={[5, 5]} />, document.getElementById('app'));
+var Game = React.createClass({
+    handleClick: function(x, y) {
+        var index = y * 8 + x;
+        var selected = (index === this.state.selected) ? null : index
+        this.setState({selected: selected});
+    },
 
-var observe = function (receive) {
-  setInterval(function () {
-    receive([
-      Math.floor(Math.random() * 8),
-      Math.floor(Math.random() * 8)
-    ]);
-  }, 500);
-};
+    getInitialState: function() {
+        var boardData = [];
+        for (let i = 0; i < 64; i++) {
+            boardData.push('');
+        }
+        boardData[0] = boardData[2] = boardData[4] = boardData[6] = 'w';
+        boardData[9] = boardData[11] = boardData[13] = boardData[15] = 'w';
 
-observe(function (pawnPosition) {
-    ReactDOM.render(<Board pawnPosition={pawnPosition} />, document.getElementById('app'));
+        var selected = null;
+
+        return { boardData: boardData, selected: selected };
+    },
+
+    render: function () {
+        return (
+            <Board
+                boardData={this.state.boardData}
+                selected={this.state.selected}
+                onClick={this.handleClick}
+            />);
+    }
 });
 
+ReactDOM.render(
+    <Game />, document.getElementById('app'));
 
 
 
