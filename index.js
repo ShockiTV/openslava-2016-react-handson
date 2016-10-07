3e87fa3e9a3968557711e1325acc5f2716011c9a
Dusan Hlavaty <dusan.hlavaty@piano.io>
2016-10-07 15:31:16 +0200

    Step 4 - drawing chessboard

--- index.js
+++ index.js
@@ -21,52 +21,138 @@
 //     document.getElementById('app')
 // );
 
-// Step 4
-function counterStore(state, action) {
-    if (typeof state === 'undefined') {
-        return 0;
-    }
-
-    switch (action.type) {
-        case 'INCREMENT':
-        return state + 1
-        case 'DECREMENT':
-        return state - 1
-        default:
-        return state;
-    }
-};
-let store = Redux.createStore(counterStore);
-
-
-
-const Message = (props) => (
-    <div>
-        <h1>Hello {props.name}, second version !</h1>
-        <p>Current count is {props.count}</p>
-    </div>
-);
-
-const mapStateToProps = (state) => {
-  return {
-    count: state
+// Step 4 - drawing an chessboard
+// I prefer to start bottom-up, because this way I'm always working with something that
+// already exists. If I were to build the Board first, I wouldn't see my results until
+// I'm done with the Square. On the other hand, I can build and see the Square right
+// away without even thinking of the Board.
+var Pawn = React.createClass({
+  render: function () {
+    return <span>&#9817;</span>;
   }
-}
-const MessageContainer = ReactRedux.connect(mapStateToProps)(Message);
+});
+
+var Square = React.createClass({
+  render: function () {
+    var fill = this.props.isBlack ? 'black' : 'white';
+    var stroke = this.props.isBlack ? 'white' : 'black';
+
+    return (
+        <div style={{
+            backgroundColor: fill,
+            color: stroke,
+            width: '100%',
+            height: '100%'
+        }}>
+            {this.props.children}
+        </div>
+    );
+  }
+});
+
+var Board = React.createClass({
+  renderSquare: function (i) {
+      var x = i % 8;
+      var y = Math.floor(i / 8);
+      var isBlack = (x + y) % 2 === 1;
+
+      var pawnX = this.props.pawnPosition[0];
+      var pawnY = this.props.pawnPosition[1];
+      var piece = (x === pawnX && y === pawnY) ? <Pawn /> : <div>&nbsp;</div>;
+
+      return (
+        <div key={i} style={{ width: '12.5%', height: '12.5%' }}>
+            <Square isBlack={isBlack}>
+              {piece}
+            </Square>
+        </div>
+      );
+  },
+
+  render: function () {
+    var squares = [];
+    for (let i = 0; i < 64; i++) {
+        squares.push(this.renderSquare(i));
+    }
+
+    return (
+        <div style={{
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            flexWrap: 'wrap'
+        }}>
+            {squares}
+      </div>
+    );
+  }
+});
+
+// ReactDOM.render(<Pawn />, document.getElementById('app'));
+// ReactDOM.render(
+//     <Board pawnPosition={[5, 5]} />, document.getElementById('app'));
+
+var observe = function (receive) {
+  setInterval(function () {
+    receive([
+      Math.floor(Math.random() * 8),
+      Math.floor(Math.random() * 8)
+    ]);
+  }, 500);
+};
+
+observe(function (pawnPosition) {
+    ReactDOM.render(<Board pawnPosition={pawnPosition} />, document.getElementById('app'));
+});
 
 
 
-const App = (props) => (
-    <ReactRedux.Provider store={store}>
-        <MessageContainer name="Openslava #3" />
-    </ReactRedux.Provider>
-);
 
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-);
+// function counterStore(state, action) {
+//     if (typeof state === 'undefined') {
+//         return 0;
+//     }
+//
+//     switch (action.type) {
+//         case 'INCREMENT':
+//         return state + 1
+//         case 'DECREMENT':
+//         return state - 1
+//         default:
+//         return state;
+//     }
+// };
+// let store = Redux.createStore(counterStore);
+//
+//
+//
+// const Message = (props) => (
+//     <div>
+//         <h1>Hello {props.name}, second version !</h1>
+//         <p>Current count is {props.count}</p>
+//     </div>
+// );
+//
+// const mapStateToProps = (state) => {
+//   return {
+//     count: state
+//   }
+// }
+// const MessageContainer = ReactRedux.connect(mapStateToProps)(Message);
+//
+//
+//
+// const App = (props) => (
+//     <ReactRedux.Provider store={store}>
+//         <MessageContainer name="Openslava #3" />
+//     </ReactRedux.Provider>
+// );
+//
+//
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('app')
+// );
 
 
 
